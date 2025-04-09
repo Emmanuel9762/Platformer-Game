7221c58b904e9ff7d26316592dba3b777ce841a3
Emmanuel <nlihleemmanuel@outlook.com>
2025-04-09 07:06:56 +0200

    instantiated new player and startG func

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Query imports
 const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
-const startScreen = document.getElementById(".start-screen");
+const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 
